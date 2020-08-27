51e0a7050bbda195f977c6804db673b5b1752065
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-08-27 16:56:17 +0300

    edit step1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.static("./public"))
 
-const PORT = procces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log("hello")
