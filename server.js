0623e404ea2846cd0e1fdb830d9a7b7dcba568a7
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-08-29 13:56:39 +0300

    edit1

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log("hello")
 })
+server.get("/test", (request , response )=>{
+    response.send("<h1>you are live</h1>")
+})
