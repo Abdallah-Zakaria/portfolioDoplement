b5cd84b80fd3d7f0c0093467331e2f2bdaa14c90
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-08-29 23:48:29 +0300

    test

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 "use strict"
 
-const PORT = process.env.PORT || 3000;
+
 
 const express = require("express");
 
 const server = express();
 
 server.use(express.static("public"))
-
-
+//
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log("hello")
