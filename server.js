64d105ed9574dfb95ee99f6927387d79413e11fd
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-08-30 00:02:35 +0300

    a

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-"use strict"
+'use strict';
 
+// make a variable called express to use the express library
+const express = require('express');
+
+// initialize the express
+// server has lot of properties and methods
+const server = express(); 
+
+// declare a PORT to connect your website with the server
 const PORT = process.env.PORT || 3000;
 
-const express = require("express");
-
-const server = express();
-
-server.use(express.static("./public"))
+// to access files and folders inside public
+server.use(express.static('./public'));
 
 
-
-server.listen(PORT, () => {
-    console.log("hello")
-})
-server.get("/test", (request , response )=>{
-    response.send("<h1>you are live</h1>")
+// http://localhost:3000/location?city=amman
+// http://localhost:3000/data
+server.get('/data',(request,response) =>{
+    response.send('You are doing great!!');
 })
 
-server.get("/help",(request,response)=> {
-    let help = [
-        {"info": "hello"},
-        {"problem": "yes"}
-    ]
-    response.json(help)
+// http://localhost:3000/family
+server.get('/family',(req,res)=>{
+    let familyArr = [
+        {name:'Atallah'},
+        {name: 'Mesina'},
+        {name:'Razan'}
+    ];
+    res.json(familyArr);
+})
+
+
+
+server.listen (PORT,()=>{
+    console.log(`listening to PORT ${PORT}`);
 })
